9509ba282b5edaf8d074bc11b4e70d5b7133beea
Christian Anderson <tehninjawaffle@gmail.com>
2023-07-12 19:50:56 -0700

    I added the useEffect functionality back in but I'm not sure if its working. got the ol 429 error, too many request. Changed some basic Css to make this readable

--- src/APOD.js
+++ src/APOD.js
@@ -8,7 +8,7 @@ const NPhoto = (props) => {
       <h1 className="header">NASA's photo of the day - {props.photo.date}</h1>
       <p className="title">{props.photo.title}</p>
       <img className="photo" src={props.photo.hdurl}></img>
-      <h3 className="explanation">{props.photo.explanation}</h3>
+      <h4 className="explanation">{props.photo.explanation}</h4>
     </div>
   )
 }
--- src/App.js
+++ src/App.js
@@ -3,31 +3,26 @@ import "./App.css";
 import axios from "axios";
 import dData from "./inCaseOfWorst";
 import NPhoto from "./APOD";
-// const apod = axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-// .then(res => {
-//   console.log(res.data);
-// })
-// .catch(err => {
-//   console.log(err);
-// })
+
+
 console.log(dData);
 function App() {
   const [pictureOfTheDay, setPictureOfTheDay] = useState(dData);
   console.log(pictureOfTheDay);
   useEffect(() => {
     // console.log("useEffect working");
-    // const apod = axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-    // .then(res => {
-    //   console.log(res.data);
-    //   return
-    // })
-    // .catch(err => {
-    //   console.log(err);
-    // })
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+    .then(res => {
+      // console.log(res.data);
+      setPictureOfTheDay(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }, [pictureOfTheDay]);
   return (
     <div className="App">
-      <NPhoto photo={dData} />
+      { pictureOfTheDay && <NPhoto photo={pictureOfTheDay} />}
     </div>
   );
 }

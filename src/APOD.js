ac554029b302f41e219e717f1f97b0bd97e7e56c
Christian Anderson <tehninjawaffle@gmail.com>
2023-07-14 08:08:19 -0700

    Added styled components as a dependency, Created the first basic style, imported it and all subsequent styles to APOD

--- src/APOD.js
+++ src/APOD.js
@@ -1,15 +1,19 @@
 import React from "react";
 // import dData from "./inCaseOfWorst"
 // console.log(dData);
+import * as styles from "./stylesforapod"
+
+
+
 const NPhoto = (props) => {
   console.log(props)
   return (
-    <div className="wrapper">
+    <styles.Wrapper className="wrapper">
       <h1 className="header">NASA's photo of the day - {props.photo.date}</h1>
       <p className="title">{props.photo.title}</p>
       <img className="photo" src={props.photo.hdurl}></img>
       <h4 className="explanation">{props.photo.explanation}</h4>
-    </div>
+    </styles.Wrapper>
   )
 }
 
---
+++ src/stylesforapod.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+import React from "react"
+
+const Wrapper = styled.div `
+max-width: 100%;
+background-color: lightgray;
+
+`
+
+export {Wrapper};

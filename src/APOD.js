f053cb58c4114a390dcf17674fc47f385952d6e9
Christian Anderson <tehninjawaffle@gmail.com>
2023-07-14 08:21:35 -0700

    I've styled multipled components, attempted a hover affect but couldn't get it to work unfortunately. The rest are very basic color changes, jsut to demonstrate I know what im supposed to be doing

--- src/APOD.js
+++ src/APOD.js
@@ -9,10 +9,10 @@ const NPhoto = (props) => {
   console.log(props)
   return (
     <styles.Wrapper className="wrapper">
-      <h1 className="header">NASA's photo of the day - {props.photo.date}</h1>
-      <p className="title">{props.photo.title}</p>
-      <img className="photo" src={props.photo.hdurl}></img>
-      <h4 className="explanation">{props.photo.explanation}</h4>
+      <styles.Header className="header">NASA's photo of the day - {props.photo.date}</styles.Header>
+      <styles.Title className="title">{props.photo.title}</styles.Title>
+      <styles.Image className="photo" src={props.photo.hdurl}></styles.Image>
+      <styles.Explanation className="explanation">{props.photo.explanation}</styles.Explanation>
     </styles.Wrapper>
   )
 }
--- src/stylesforapod.js
+++ src/stylesforapod.js
@@ -3,8 +3,30 @@ import React from "react"
 
 const Wrapper = styled.div `
 max-width: 100%;
-background-color: lightgray;
+background-color: burlywood;
 
 `
+const Header = styled.h1 `
+color: cornsilk
 
+`
+const Title = styled.p `
+color: mistyrose;
+`
+const Image = styled.img `
+width: 70%
+&:hover {
+  width: 95%;
+}
+//not working, not sure why
+`
+
+const Explanation = styled.h4 `
+color: black;
+
+`
+export {Explanation};
+export {Image};
+export {Title};
+export {Header};
 export {Wrapper};
